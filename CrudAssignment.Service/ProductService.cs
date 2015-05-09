5d7a82ec3db8d82fae8b67627e5128e39520ab0e
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-09 18:32:54 +0200

    Product service with interface

---
+++ CrudAssignment.Service/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrudAssignment.Entities.Models;
+using Repository.Pattern.Repositories;
+using Service.Pattern;
+
+namespace CrudAssignment.Service
+{
+    public interface IProductService : IService<Product>
+    {
+    }
+
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IRepositoryAsync<Product> _productRepository;
+
+        public ProductService(IRepositoryAsync<Product> productRepository) : base(productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
