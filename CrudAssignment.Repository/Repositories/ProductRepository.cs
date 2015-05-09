892522ccc18198ce6c58d140ca289a751b8b19f7
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-09 22:14:59 +0200

    Added custom methods to ProductRepository, implemented it in ProductService

---
+++ CrudAssignment.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrudAssignment.Entities.Models;
+using Repository.Pattern.Repositories;
+
+namespace CrudAssignment.Repository.Repositories
+{
+    public static class ProductRepository
+    {
+        public static IEnumerable<Product> GetProductsOrderByName(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.Name);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.Name);
+            }
+        }
+
+        public static IEnumerable<Product> GetProductsOrderByCategory(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.Category);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.Category);
+            }
+        }
+
+        public static IEnumerable<Product> GetProductsOrderBySupplier(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.Supplier);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.Supplier);
+            }
+        }
+
+        public static IEnumerable<Product> GetProductsOrderByPrice(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.Price);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.Price);
+            }
+        }
+
+        public static IEnumerable<Product> GetProductsOrderByDeliveryPeriod(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.DeliveryPeriod);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.DeliveryPeriod);
+            }
+        }
+
+        public static IEnumerable<Product> GetProductsOrderByMinimumStock(this IRepositoryAsync<Product> repository, bool ascending = true)
+        {
+            if (ascending)
+            {
+                return repository.Queryable().OrderBy(p => p.MinimumStock);
+            }
+            else
+            {
+                return repository.Queryable().OrderByDescending(p => p.MinimumStock);
+            }
+        }
+    }
+}
--- CrudAssignment.Service/ProductService.cs
+++ CrudAssignment.Service/ProductService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CrudAssignment.Entities.Models;
+using CrudAssignment.Repository.Repositories;
 using Repository.Pattern.Repositories;
 using Service.Pattern;
 
@@ -20,6 +21,36 @@ namespace CrudAssignment.Service
         public ProductService(IRepositoryAsync<Product> productRepository) : base(productRepository)
         {
             _productRepository = productRepository;
+        }
+
+        public IEnumerable<Product> GetProductsOrderByName(bool ascending = true)
+        {
+            return _productRepository.GetProductsOrderByName(ascending);
+        }
+
+        public IEnumerable<Product> GetProductsOrderByCategory(bool ascending = true)
+        {
+            return _productRepository.GetProductsOrderByCategory(ascending);
+        }
+
+        public IEnumerable<Product> GetProductsOrderBySupplier(bool ascenging = true)
+        {
+            return _productRepository.GetProductsOrderBySupplier(ascenging);
+        }
+
+        public IEnumerable<Product> GetProductsOrderByPrice(bool ascending = true)
+        {
+            return _productRepository.GetProductsOrderByPrice(ascending);
+        }
+
+        public IEnumerable<Product> GetProductsOrderByDeliveryPeriod(bool ascending = true)
+        {
+            return _productRepository.GetProductsOrderByDeliveryPeriod(ascending);
+        }
+
+        public IEnumerable<Product> GetProductsOrderByMinimumStock(bool ascending = true)
+        {
+            return _productRepository.GetProductsOrderByMinimumStock(ascending);
         }
     }
 }
