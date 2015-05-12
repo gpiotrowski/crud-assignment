88da13f38af55a431454d7a8cc78e461cbc411cc
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-12 14:53:26 +0200

    Custom validation message

---
+++ CrudAssignment.Web/Helpers/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace CrudAssignment.Web.Helpers
+{
+    public static class Validator
+    {
+        public static MvcHtmlString CustomValidationMessageFor<TModel, TProperty>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> expression)
+        {
+            var containerDivBuilder = new TagBuilder("div");
+            containerDivBuilder.GenerateId("validation-message");
+
+            var leftDivBuilder = new TagBuilder("div");
+            leftDivBuilder.AddCssClass("error-left");
+
+            var innerDivBuilder = new TagBuilder("div");
+            innerDivBuilder.AddCssClass("error-inner");
+            innerDivBuilder.InnerHtml = helper.ValidationMessageFor(expression).ToString();
+
+            containerDivBuilder.InnerHtml += leftDivBuilder.ToString(TagRenderMode.Normal);
+            containerDivBuilder.InnerHtml += innerDivBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(containerDivBuilder.ToString(TagRenderMode.Normal));
+        }
+    }
+}
