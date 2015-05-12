f3454c94f76faf5e0a4a677e8ab6fda70b8992b9
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-12 14:52:46 +0200

    All product fields are required

--- CrudAssignment.Entities/Models/Product.cs
+++ CrudAssignment.Entities/Models/Product.cs
@@ -13,12 +13,18 @@ namespace CrudAssignment.Entities.Models
     public class Product : Entity
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public int DeliveryPeriod { get; set; }
+        [Required]
         public int MinimumStock { get; set; }
+        [Required]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+        [Required]
         public int SupplierId { get; set; }
         public virtual Supplier Supplier { get; set; }
     }
