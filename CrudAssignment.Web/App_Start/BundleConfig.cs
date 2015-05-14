ebeee0713f4245ae333a839d6c0f05fc437c58e0
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-14 12:30:20 +0200

    Display value helper (1 day, 2 days, 2.00 € etc.)

--- CrudAssignment.Web/App_Start/BundleConfig.cs
+++ CrudAssignment.Web/App_Start/BundleConfig.cs
@@ -27,6 +27,10 @@ namespace CrudAssignment.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/helpers").Include(
+                    "~/Scripts/helpers.js",
+                    "~/Scripts/displayHelpers.js"));
         }
     }
 }
---
+++ CrudAssignment.Web/Scripts/displayHelpers.js
@@ -0,0 +1,8 @@
+﻿function displayMoney(value) {
+    return value.formatMoney(2, ",", ".") + " €";
+}
+
+function displayDays(value) {
+    var form = (value != 1 ? "days" : "day");
+    return value + " " + form;
+}
