a8b51fd059bde0c2e8f18be1a3f413b150157cdd
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-14 12:35:31 +0200

    Custom sorting in DataTable, e.g. 20 days, 5 days - sort as a number (first 5 days, then 20 days) not as a text value (in this case it will be first 20 days and then 5 days which is invalid)

---
+++ CrudAssignment.Web/Scripts/jquery.dataTables.customSorting.js
@@ -0,0 +1,31 @@
+﻿// Sorting by days column (1 day, 2 days etc.)
+(function () {
+    
+    // Change this list to the valid characters you want
+    // Init the regex just once for speed - it is "closure locked"
+    var re = new RegExp("[0123456789]+ days?");
+
+    jQuery.fn.dataTableExt.aTypes.unshift(
+        function (data) {
+             if (typeof data !== 'string' || !re.test(data)) {
+                 return null;
+             }
+             return 'day-displayer';
+        }
+    );
+
+}());
+
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+    "day-displayer-asc": function (a, b) {
+        a = parseInt(a.split(" ")[0]);
+        b = parseInt(b.split(" ")[0]);
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "day-displayer-desc": function (a, b) {
+        a = parseInt(a.split(" ")[0]);
+        b = parseInt(b.split(" ")[0]);
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+});
