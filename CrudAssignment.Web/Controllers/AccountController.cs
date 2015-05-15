d0014f254d557981f52d9fc36223640893395eab
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-15 14:49:51 +0200

    Top navigation bar

--- CrudAssignment.Web/Controllers/AccountController.cs
+++ CrudAssignment.Web/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace CrudAssignment.Web.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
@@ -449,7 +449,7 @@ namespace CrudAssignment.Web.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Product");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
