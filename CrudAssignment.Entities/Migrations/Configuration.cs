9c7d5a7533136d472cac8bd9ad50ee86a0330415
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2015-05-17 16:22:32 +0200

    Initial seed method

--- CrudAssignment.Entities/Migrations/Configuration.cs
+++ CrudAssignment.Entities/Migrations/Configuration.cs
@@ -1,3 +1,7 @@
+using CrudAssignment.Entities.Models;
+using Microsoft.AspNet.Identity;
+using Repository.Pattern.Infrastructure;
+
 namespace CrudAssignment.Entities.Migrations
 {
     using System;
@@ -26,6 +30,78 @@ namespace CrudAssignment.Entities.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var passwordHash = new PasswordHasher();
+            context.AspNetUsers.AddOrUpdate(u => u.UserName,
+                new AspNetUser
+                {
+                    Id = "f5df20ea-4c5d-450e-b678-2adfe7111a54",
+                    Email = "user@test.contoso",
+                    EmailConfirmed = false,
+                    PasswordHash = passwordHash.HashPassword("Pa$$w0rd"),
+                    SecurityStamp = "6254a792-95f0-4625-a60e-7f43e6d045b2",
+                    PhoneNumberConfirmed = false,
+                    LockoutEnabled = true,
+                    AccessFailedCount = 0,
+                    UserName = "TestUser",
+                    ObjectState = ObjectState.Added
+                }
+            );
+
+            context.Categories.AddOrUpdate(c => c.Name,
+                new Category
+                {
+                    Id = 1,
+                    Name = "Category 1",
+                    ObjectState = ObjectState.Added
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Category 2",
+                    ObjectState = ObjectState.Added
+                }
+            );
+
+            context.Suppliers.AddOrUpdate(s => s.Name,
+                new Supplier
+                {
+                    Id = 1,
+                    Name = "Supplier 1",
+                    ObjectState = ObjectState.Added
+                },
+                new Supplier
+                {
+                    Id = 2,
+                    Name = "Supplier 2",
+                    ObjectState = ObjectState.Added
+                }
+            );
+
+            context.Products.AddOrUpdate(p => p.Id,
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Name = "milk",
+                    Price = new decimal(1.00),
+                    DeliveryPeriod = 2,
+                    SupplierId = 1,
+                    MinimumStock = 25,
+                    ObjectState = ObjectState.Added
+                },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 2,
+                    Name = "bread",
+                    Price = new decimal(2.00),
+                    DeliveryPeriod = 1,
+                    SupplierId = 2,
+                    MinimumStock = 60,
+                    ObjectState = ObjectState.Added
+                }
+            );
         }
     }
 }
